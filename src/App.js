7b33cb1f9866b0b1f8af65208c3c23c9fc0c64e3
Sampaguitas <timothee.desurmont@sampaguitas.com>
2019-01-09 01:40:00 +0400

    first components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Select from './Select';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      opportunity: {
+        territory: '',
+        industry: '',
+      },
+      territories: [],
+      industries: [],
+      submitted: false
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    const { opportunity } = this.state;
+    this.setState({
+      opportunity: {
+        ...opportunity,
+        [name]: value
+      }
+    });
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({ submitted: true });
+    const { opportunity } = this.state;
+    if (opportunity.territory && opportunity.industry) {
+      console.log('do something');
+    }
+  }
   render() {
+    const { opportunity, territories, industries, submitted } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div classNmae="container d-flex h-100">
+        <div className='row justify-content-center align-self-center'>
+          <div className="card" style={{width: 800}}>
+            <div className="card-header" style={{ backgroundColor: '#180F5E', height: '4em', textAlign: 'center'}}>
+              <h5 style={{ color: '#FFFFFF'}}>Generate a new code</h5>
+          </div>
+          <div className="card-body">
+          <form name="form" onSubmit={this.handleSubmit}>
+                  <Select
+                    title="Territory"
+                    name="territory"
+                    options={territories.items}
+                    value={opportunity.territories}
+                    onChange={this.handleChange}
+                    placeholder=""
+                    submitted={submitted}
+                    inline={false}
+                    required={true}
+                  />
+                  <Select
+                    title="Industry"
+                    name="industry"
+                    options={industries.items}
+                    value={opportunity.industry}
+                    onChange={this.handleChange}
+                    placeholder=""
+                    submitted={submitted}
+                    inline={false}
+                    required={true}
+                  />
+          </form>
+
+          </div>
+        </div>
+      </div>
       </div>
     );
   }
---
+++ src/Select.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class Select extends Component {
+    render() {
+        return (
+            <div className={'form-group' + (this.props.inline ? ' row' : '') + (this.props.submitted && this.props.required && !this.props.value ? ' has-error' : '')}>
+                <label
+                    htmlFor={this.props.name}
+                    className={this.props.inline ? "col-sm-2 col-form-label" : ''}
+                >{this.props.title}</label>
+                <div className={this.props.inline ? "col-sm-10" : ''}>
+                    <select
+                        className="form-control"
+                        type="text"
+                        id={this.props.name}
+                        name={this.props.name}
+                        value={this.props.value}
+                        onChange={this.props.onChange}
+                        style={{height: '2.7em'}}
+                    >
+                        <option defaultValue="" disabled hidden>{this.props.placeholder}</option>
+                        {this.props.options && this.props.options.map(option => {
+                            return (
+                                <option
+                                    key={option._id}
+                                    value={option._id}>{option.name}
+                                </option>
+                            );
+                        })}
+                    </select>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default Select;
