0b28c0fa7c3fb33ff544ece0409b335c4f7c2070
Sampaguitas <timothee.desurmont@sampaguitas.com>
2019-01-09 12:14:24 +0400

    api calls

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import Select from './Select';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
+library.add(fas);
+
 
 class App extends Component {
   constructor(props) {
@@ -12,10 +17,24 @@ class App extends Component {
       },
       territories: [],
       industries: [],
-      submitted: false
+      submitted: false,
+      loading: false,
+      message:'',
+      salesforce:'',
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.getTerritories = this.getTerritories.bind(this);
+    this.getIndustries = this.getIndustries.bind(this);
+  }
+  componentDidMount(){
+    const that = this;
+    this.getIndustries().then(function (response) {
+      that.setState({ industries: response });
+    });
+    this.getTerritories().then(function (response) {
+      that.setState({ territories: response });
+    });
   }
 
   handleChange(event) {
@@ -27,41 +46,92 @@ class App extends Component {
         [name]: value
       }
     });
+    console.log(this.state);
   }
   handleSubmit(event) {
     event.preventDefault();
     this.setState({ submitted: true });
     const { opportunity } = this.state;
+    const that = this
     if (opportunity.territory && opportunity.industry) {
-      console.log('do something');
+      this.createOpportunity(opportunity).then(function (response) {
+        that.setState({ salesforce: response });
+        console.log(response);
+      });
+    } else {
+      console.log('empty');
     }
+      
   }
+
+  getIndustries() {
+    const requestOptions = {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' }
+    };
+    return fetch(`https://opportunity-server.herokuapp.com/industry/findAll`, requestOptions).then(this.handleResponse);
+  }
+
+  getTerritories() {
+    const requestOptions = {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' }
+    };
+    return fetch(`https://opportunity-server.herokuapp.com/territory/findAll`, requestOptions).then(this.handleResponse);
+  }
+
+  createOpportunity(opportunity) {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(opportunity)
+    };
+    return fetch(`https://opportunity-server.herokuapp.com/opportunity/create`, requestOptions).then(this.handleResponse);
+  }
+
+  handleResponse(response) {
+    return response.text().then(text => {
+      const data = text && JSON.parse(text);
+      if (!response.ok) {
+        if (response.status === 401) {
+          // auto logout if 401 response returned from api
+        }
+        const error = (data && data.message) || response.statusText;
+        return Promise.reject(error);
+      }
+      return data;
+    });
+  }
+
   render() {
-    const { opportunity, territories, industries, submitted } = this.state
+    const { salesforce, industries, loading, opportunity, submitted, territories } = this.state
     return (
       <div classNmae="container d-flex h-100">
         <div className='row justify-content-center align-self-center'>
           <div className="card" style={{width: 800}}>
-            <div className="card-header" style={{ backgroundColor: '#180F5E', height: '4em', textAlign: 'center'}}>
-              <h5 style={{ color: '#FFFFFF'}}>Generate a new code</h5>
+            <div className="card-header" style={{ textAlign: 'center'}}>
+              <h5>Generate a new code</h5>
           </div>
           <div className="card-body">
           <form name="form" onSubmit={this.handleSubmit}>
+                {this.state.territories &&
                   <Select
                     title="Territory"
                     name="territory"
-                    options={territories.items}
-                    value={opportunity.territories}
+                    options={territories}
+                    value={opportunity.territory}
                     onChange={this.handleChange}
                     placeholder=""
                     submitted={submitted}
                     inline={false}
                     required={true}
                   />
+                }
+                {this.state.industries &&
                   <Select
                     title="Industry"
                     name="industry"
-                    options={industries.items}
+                    options={industries}
                     value={opportunity.industry}
                     onChange={this.handleChange}
                     placeholder=""
@@ -69,8 +139,22 @@ class App extends Component {
                     inline={false}
                     required={true}
                   />
-          </form>
+                }
 
+            <hr />
+            <div>
+                  <blockquote className="blockquote text-center"><p className="mb-0" style={{ fontSize: 30, fontWeight: 'bold' }}>{salesforce.code}</p></blockquote>     
+            </div>
+            <hr />
+            <button 
+              type="submit"
+              className="btn btn-primary btn-lg btn-block"
+              style={{ backgroundColor: "#A8052C"}}
+            >
+              {loading && <FontAwesomeIcon icon="spinner" className="fa-pulse fa-1x fa-fw" />}
+              Apply
+            </button>
+          </form>
           </div>
         </div>
       </div>
